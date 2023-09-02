25b4132a8cdcfb0538c603dc7d8915c22abce6c6
Jenghiz Khan <136552882+jenghiz-khan@users.noreply.github.com>
2023-09-02 09:04:20 +0700

    Add files via upload

--- main.go
+++ main.go
@@ -9,10 +9,12 @@ import (
 	"strings"
 )
 
+// Struct participants json
 type Participants struct {
 	Participants []student `json:"participants"`
 }
 
+// struct student
 type student struct {
 	Id        string `json:"id"`
 	Code      string `json:"student_code"`
@@ -22,32 +24,36 @@ type student struct {
 	Alasan    string `json:"joining_reason"`
 }
 
-func searchStudent(kode string, arrayOfParticipants []student) (student, error) {
-	kode = strings.ToLower(kode)
-	var theRightStudent student
-	for _, chosenStudent := range arrayOfParticipants {
-		if strings.ToLower(chosenStudent.Code) == kode {
-			theRightStudent = chosenStudent
-			return theRightStudent, nil
+// get data student by Kode Peserta
+func getStudent(code string, arrayOfParticipants []student) (student, error) {
+	code = strings.ToLower(code)
+	var dataStudent student
+	for _, readStudent := range arrayOfParticipants {
+		if strings.ToLower(readStudent.Code) == code {
+			dataStudent = readStudent
+			return dataStudent, nil
 		}
 	}
-	return theRightStudent, errors.New("Data Not Found")
+	return dataStudent, errors.New("Data Not Found")
 
 }
 
-func checkError(err error) {
+// error function
+func errorCheck(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
 }
+
+// function main untuk membaca file json yang akan menampilkan data student
 func main() {
 	jsonByte, err := os.ReadFile("participants.json")
-	checkError(err)
+	errorCheck(err)
 
 	var data Participants
-	checkError(json.Unmarshal(jsonByte, &data))
+	errorCheck(json.Unmarshal(jsonByte, &data))
 
-	student, err := searchStudent(os.Args[1], data.Participants)
+	student, err := getStudent(os.Args[1], data.Participants)
 	if err != nil {
 		fmt.Println(err)
 		return
